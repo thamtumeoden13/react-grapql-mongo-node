721e289d0b9521f53dff420703b1cdb682c97e91
ThamTuMeoDen13 <detective.blackcat13@gmail.com>
2020-05-19 23:51:17 +0700

    build schema graphql

--- app.js
+++ app.js
@@ -1,12 +1,39 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send('hello word')
-})
+app.use('/graphql',
+    graphqlHttp({
+        schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            events: () => {
+                return ['aaaaaaaa', 'bbbbbbbb', 'cccccccc', 'ddddddd'];
+            },
+            createEvent: (args) => {
+                const eventName = args.name;
+                return eventName;
+            }
+        },
+        graphiql: true
+    })
+);
 
 app.listen(3000);
