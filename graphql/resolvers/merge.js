081ab7377aca1a74530543ae1bb9389f21c3e3c4
ThamTuMeoDen13 <detective.blackcat13@gmail.com>
2020-06-22 08:42:18 +0700

    finish

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -32,9 +32,15 @@ const transformBooking = booking => {
     }
 }
 
-const events = async eventId => {
+const events = async eventIds => {
     try {
-        const events = await Event.find({ _id: { $in: eventId } });
+        const events = await Event.find({ _id: { $in: eventIds } });
+        events.sort((a, b) => {
+            return (
+                eventIds.indexOf(a._id.toString() - eventIds.indexOf(b._id.toString()))
+            )
+        })
+        console.log(eventIds, events)
         return events.map(event => {
             return transformEvent(event)
         })
